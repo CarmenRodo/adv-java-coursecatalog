d2d6e83320e3d2bd9c43035b1c7d43c78e12a6f3
Rodolfo <carmen.rodolfo2003@gmail.com>
2023-12-05 17:46:08 -0600

    initial Commit

--- src/lab1/IntroToProgrammingCourse.java
+++ src/lab1/IntroToProgrammingCourse.java
@@ -6,7 +6,7 @@ import javax.swing.*;
  * Describe responsibilities here. NOTE: this class has NO PREREQUISITES! Do not
  * change this fact.
  *
- * @author your name goes here
+ * @Rodolfo your name goes here
  * @version 1.00
  */
 public class IntroToProgrammingCourse {
--- src/lab2/IntroToProgrammingCourse.java
+++ src/lab2/IntroToProgrammingCourse.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 /**
  * Describe responsibilities here.
  *
- * @author your name goes here
+ * @Rodolfo your name goes here
  * @version 1.00
  */
 public class IntroToProgrammingCourse {
